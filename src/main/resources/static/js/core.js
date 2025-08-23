67edbffb8953c4a98f02f5af20db8722124458d8
Ronni Kahalani <ronni.kahalani@gmail.com>
2025-08-23 14:37:19 +0200

    Adding js event integration

--- src/main/resources/static/js/core.js
+++ src/main/resources/static/js/core.js
@@ -1,1 +1,31 @@
-alert("Hi there! This is a test alert from core.js.");
+/**
+ * Core JavaScript functions for navigation in the application
+ */
+
+/**
+ * Function to navigate to the home page
+ */
+function home() {
+    location.href = "/";
+}
+
+/**
+ * Function to navigate to the user list page
+ */
+function listUsers() {
+    location.href = "/users";
+}
+
+/**
+ * Function to navigate to the user form page
+ */
+function addUser() {
+    location.href = "/form";
+}
+
+/**
+ * Event listeners for navigation buttons
+ */
+document.getElementById("btnHome").addEventListener("click", home);
+document.getElementById("btnListUsers").addEventListener("click", listUsers);
+document.getElementById("btnAddUser").addEventListener("click", addUser);
