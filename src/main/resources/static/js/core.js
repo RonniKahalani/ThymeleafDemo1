fb42e36e7cf3040f65bee7c94ba72aa88e94624b
Ronni Kahalani <ronni.kahalani@gmail.com>
2025-08-28 19:44:56 +0200

    initial changes

--- src/main/resources/static/js/core.js
+++ src/main/resources/static/js/core.js
@@ -2,3 +2,20 @@
  * Core JavaScript functions for navigation in the application
  */
 
+// Another way to select elements.
+/**
+const btnHome = document.getElementById('btnNavHome');
+const btnListUsers = document.getElementById('btnNavListUsers');
+const btnCreateUser = document.getElementById('btnNavCreateUser');
+const btnSettings = document.getElementById('btnNavSettings');
+*/
+
+const btnHome = document.querySelector("#btnNavHome");
+const btnListUsers = document.querySelector("#btnNavListUsers");
+const btnCreateUser = document.querySelector("#btnNavCreateUser");
+const btnSettings = document.querySelector("#btnNavSettings");
+
+btnHome.onclick = ()=> window.location.href = "/";
+btnListUsers.onclick = ()=> window.location.href = "/user";
+btnCreateUser.onclick = ()=> window.location.href = "/user/create";
+btnSettings.onclick = ()=> alert("Settings page is under construction.");
