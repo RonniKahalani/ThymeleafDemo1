cfd8238565310779c280c9dcad442289e3ed1df5
Ronni Kahalani <ronni.kahalani@gmail.com>
2025-08-29 08:25:02 +0200

    initial changes

--- src/main/resources/static/js/core.js
+++ src/main/resources/static/js/core.js
@@ -15,10 +15,43 @@ const btnListUsers = document.querySelector("#btnNavListUsers");
 const btnCreateUser = document.querySelector("#btnNavCreateUser");
 const btnSettings = document.querySelector("#btnNavSettings");
 
-btnHome.onclick = ()=> window.location.href = "/";
-btnListUsers.onclick = ()=> window.location.href = "/user";
-btnCreateUser.onclick = ()=> window.location.href = "/user/create";
-btnSettings.onclick = ()=> underConstruction();
+/*
+ * Here are some examples of onclick event handling done in different ways.
+
+// With an explicit inline anonymous function definition.
+btnHome.onclick = function (ev) {
+    openUrl("/");
+};
+
+// With an arrow function definition and the inline action. Because there is only onli line of code, you could remove the brackets.
+btnListUsers.onclick = (ev)=> {
+    window.location.href = "/user";
+}
+
+// With an arrow function definition calling another function.
+btnCreateUser.onclick = (ev)=> openUrl("/user/create");
+
+// With a direct function reference.
+btnSettings.onclick = underConstruction;
+
+function openUrl(url) {
+    window.location.href = url;
+}
+ */
+
+// This is how I would do it
+btnHome.onclick = (ev) => openUrl("/");
+btnListUsers.onclick = (ev)=> open("/user");
+btnCreateUser.onclick = (ev)=> openUrl("/user/create");
+btnSettings.onclick = (ev) => underConstruction();
+
+/**
+ * Open a URL in the current window
+ * @param url
+ */
+function openUrl(url) {
+    window.location.href = url;
+}
 
 /**
  * Alert for features under construction
