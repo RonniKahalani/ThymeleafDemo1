c00643aec081890822f2c2d9529546a519b1dad1
Ronni Kahalani <ronni.kahalani@gmail.com>
2025-08-29 13:52:58 +0200

    initial changes

--- src/main/java/com/example/thymeleafdemo1/home/controller/HomeController.java
+++ src/main/java/com/example/thymeleafdemo1/home/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.example.thymeleafdemo1.home.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +20,9 @@ public class HomeController {
     private String TITLE_APP_NAME;
 
     private final List<String> marquees = List.of(
-            "This marquee is made with CSS animation and scrolls through a text defined in the HTML file, under a tag with the CSS class \\\"marquee\\\" defined.",
-            "Great offers, don't miss out!",
-            "New arrivals just in!");
+            "<span class='marquee-item-introduction'>INTRODUCTION</span> This marquee is made with CSS animation and scrolls through a text defined in the HTML file, under a tag with the CSS class 'marquee' defined.",
+            "<span class='marquee-item-promotion'>PROMOTION</span> Great offers, don't miss out! Check our website for more details. Limited time only! Hurry up!",
+            "<span class='marquee-item-product'>PRODUCT</span> New arrivals just in! Explore our latest collection of products and find something you love.");
 
     /**
      * This is the controller that handles the root URL and returns the start page.
@@ -32,8 +31,7 @@ public class HomeController {
      */
     @RequestMapping("/")
     public String index(Model viewModel) throws FileNotFoundException {
-
-        viewModel.addAttribute(VIEW_ATTR_MARQUEE, marquees.get(0));
+        viewModel.addAttribute(VIEW_ATTR_MARQUEE, String.join(" - ", marquees));
         viewModel.addAttribute(VIEW_ATTR_PAGE_TITLE, TITLE_APP_NAME);
         return VIEW_HOME;
     }
--- src/main/resources/static/js/core.js
+++ src/main/resources/static/js/core.js
@@ -2,24 +2,60 @@
  * Core JavaScript functions for navigation in the application
  */
 
-// Another way to select elements.
 /**
+ // Another way to select elements.
 const btnHome = document.getElementById('btnNavHome');
 const btnListUsers = document.getElementById('btnNavListUsers');
 const btnCreateUser = document.getElementById('btnNavCreateUser');
 const btnSettings = document.getElementById('btnNavSettings');
 */
 
+/*
+ Setting button elements, using document.querySelector functions.
+ */
 const btnHome = document.querySelector("#btnNavHome");
 const btnListUsers = document.querySelector("#btnNavListUsers");
 const btnCreateUser = document.querySelector("#btnNavCreateUser");
 const btnSettings = document.querySelector("#btnNavSettings");
 
-// Using => arrow functions to set button event handlers.
+/*
+ Setting button onclick event handlers, using => arrow functions.
+ */
 btnHome.onclick = (ev) => window.location.href = "/";
 btnListUsers.onclick = (ev)=> window.location.href = "/user";
 btnCreateUser.onclick = (ev)=> window.location.href = "/user/create";
 btnSettings.onclick = (ev) => underConstruction();
+
+/*
+ Setting button onmouseover event handlers, using => arrow functions.
+ */
+btnHome.onmouseover = (ev) => setFocus(btnHome, true);
+btnListUsers.onmouseover = (ev) => setFocus(btnListUsers, true);
+btnCreateUser.onmouseover = (ev) => setFocus(btnCreateUser, true);
+btnSettings.onmouseover = (ev) => setFocus(btnSettings, true);
+
+/*
+ Setting button onmouseout event handlers, using => arrow functions.
+ */
+btnHome.onmouseout = (ev) => setFocus(btnHome, false);
+btnListUsers.onmouseout = (ev) => setFocus(btnListUsers, false);
+btnCreateUser.onmouseout = (ev) => setFocus(btnCreateUser, false);
+btnSettings.onmouseout = (ev) => setFocus(btnSettings, false);
+
+/**
+ * Set focus style on element
+ * @param element
+ * @param focus
+ */
+function setFocus(element, focus=true) {
+    if(!focus) {
+        element.style.removeProperty("background-color");
+        element.style.removeProperty("color");
+        return;
+    }
+    element.style.backgroundColor = "#4CAF50";
+    element.style.color = "white";
+}
 
 /**
  * Alert for features under construction
